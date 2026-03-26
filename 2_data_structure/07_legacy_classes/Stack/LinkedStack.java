5a8fdeab48b7d44c5eaf22811c3435bb842cc60c
Sujon <abdur.sujon.dev@gmail.com>
2026-03-26 22:15:14 +0000

    LinkedStack added

---
+++ 2_data_structure/07_legacy_classes/Stack/LinkedStack.java
@@ -0,0 +1,53 @@
+package Stack;
+
+import LinkedList.LinkNode;
+
+import java.util.EmptyStackException;
+
+public class LinkedStack implements Stack {
+    protected LinkNode top;
+
+    public LinkedStack(){
+        top = null;
+    }
+
+    @Override
+    public boolean isEmpty() {
+        return top == null;
+    }
+
+    @Override
+    public Object peek() {
+       if(isEmpty()) throw new EmptyStackException();
+       return top.theObject;
+    }
+
+    @Override
+    public void push(Object theObject) {
+        top = new LinkNode(theObject, top);
+    }
+
+    @Override
+    public Object pop() {
+        if(isEmpty()) throw new EmptyStackException();
+        Object topObject = top.theObject;
+        top = top.next;
+        return topObject;
+    }
+
+    public static void main(String[] args){
+        LinkedStack s = new LinkedStack();
+        s.push(1);
+        s.push(2);
+        s.push(3);
+        s.push(4);
+
+        while(!s.isEmpty()){
+            System.out.println("Top element is " + s.peek());
+            System.out.println("Removed item is " + s.pop());
+        }
+
+        System.out.println(s.isEmpty());
+    }
+
+}
