c294dd09c0501a2bb4e901f226d167f11f6a07f4
Sujon <abdur.sujon.dev@gmail.com>
2026-03-26 03:02:09 +0000

    Stack interface added

---
+++ 2_data_structure/07_legacy_classes/Stack/ArrayStack.java
@@ -0,0 +1,54 @@
+package Stack;
+
+import java.util.EmptyStackException;
+
+public class ArrayStack implements Stack{
+    int top;
+    Object[] stack;
+
+    public ArrayStack(int initialCapacity){
+        if(initialCapacity < 0){
+            throw new IllegalArgumentException("Capacity must be >= 1");
+        }
+        stack = new Object[initialCapacity];
+    }
+
+    public ArrayStack(){
+        this(10);
+    }
+
+    @Override
+    public boolean isEmpty() {
+        return top == -1; // return true if list is empty
+    }
+
+    @Override
+    public Object peek() {
+        if(isEmpty()){
+            throw new EmptyStackException();
+        }
+        return stack[top];
+    }
+
+    @Override
+    public void push(Object theObject) {
+        if(top == stack.length - 1){
+            Object[] newArray = new Object[stack.length * 2];
+            System.arraycopy(stack, 0, newArray, 0, stack.length);
+            stack = newArray;
+        }
+
+        // Put the new element on top of the stack
+        stack[++top] = theObject;
+    }
+
+    @Override
+    public Object pop() {
+        if(isEmpty()){
+            throw new EmptyStackException();
+        }
+        Object topObject = stack[top];
+        stack[top--] = null; // allow garbage collection
+        return topObject;
+    }
+}
